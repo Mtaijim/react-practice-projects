afab74d6b48a8cdf16e8640138f8bfece3f986e0
Mtaijim shaikh <mhdtaijim07@gmail.com>
2026-04-03 12:17:30 +0530

    weather app

--- weather-app/src/api/weatherApi.js
+++ weather-app/src/api/weatherApi.js
@@ -1,9 +1,9 @@
-const API_KEY = "90ec140ed0dbee565e9c11eb3a332fdb";
+const API_KEY = "abcd";
 const url = "https://api.openweathermap.org/data/2.5";
 
 export async function getWeather(city) {
   const res = await fetch(
-    `${url}/weather?q=${city}&appid=${API_KEY}&units=metric`
+    `${url}/weather?q=${city}&appid=${API_KEY}&units=metric`,
   );
   if (!res.ok) throw new Error("city not found");
   return res.json();
